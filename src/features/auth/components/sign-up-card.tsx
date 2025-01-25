6a864c136eaf28f72b24d4b98951e29d29328691
MohammedAdnan <MohammedAdnan.cs@hotmail.com>
2025-01-25 16:19:59 +0200

    Sidebar-NavBar

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -19,7 +19,7 @@ import { useRegister } from "../api/use-register";
 
 export const SignUpCard = () => {
 
-  const {mutate} = useRegister();
+  const {mutate , isPending} = useRegister();
 
   const form = useForm<z.infer<typeof registerSchema>>({
       resolver: zodResolver(registerSchema),
@@ -100,7 +100,7 @@ export const SignUpCard = () => {
                 </FormItem>
               )} 
             />
-            <Button disabled={false} size="lg" className="w-full">
+            <Button disabled={isPending} size="lg" className="w-full">
               Register
             </Button>
           </form>
@@ -110,11 +110,11 @@ export const SignUpCard = () => {
         <DottedSeparator />
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
-        <Button disabled={false} variant="secondary" size="lg" className="w-full">
+        <Button disabled={isPending} variant="secondary" size="lg" className="w-full">
           <FcGoogle className="mr-2 size-5" />
           Login with Google
         </Button>
-        <Button disabled={false} variant="secondary" size="lg" className="w-full">
+        <Button disabled={isPending} variant="secondary" size="lg" className="w-full">
           <FaGithub className="mr-2 size-5" />
           Login with Github
         </Button>
