d6092f360dba2ba74f0ba9dd23df05fb19141c42
AndrewHabel <andrewmagdy545@gmail.com>
2025-01-30 15:54:52 +0200

    projects page 1

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,10 +7,9 @@ import { RiAddCircleFill } from "react-icons/ri";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 export const Projects = () => {
-    
-    const ProjectId = null;
     const pathname = usePathname();
     const {open} =  useCreateProjectModal();
     const workspaceId = useWorkspaceId();
@@ -23,7 +22,7 @@ export const Projects = () => {
                 <RiAddCircleFill onClick={open} className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition " />
               </div>
               {data?.documents.map((project) => {
-                const href = `/workspaces/${workspaceId}/projects/${project.id}`;
+                const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
                 const isActive = pathname === href;
                 return (
                   <Link href={href} key={project.$id}>
@@ -31,6 +30,7 @@ export const Projects = () => {
                         "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500 ",
                         isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
                     )}>
+                        <ProjectAvatar image={project.imageUrl} name={project.name} />
                         <span className="truncate">{project.name}</span>
                     </div>
                   </Link>
